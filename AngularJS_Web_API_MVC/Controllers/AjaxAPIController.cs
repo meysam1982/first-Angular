ab7bb12e522d00ee2844200e5dad84c5fcfb1561
meysam ahmadi <meysam.ahmadi82@gmail.com>
2018-11-05 20:47:25 -0800

    first commit

---
+++ AngularJS_Web_API_MVC/Controllers/AjaxAPIController.cs
@@ -0,0 +1,23 @@
+﻿using AngularJS_Web_API_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AngularJS_Web_API_MVC.Controllers
+{
+    public class AjaxAPIController : ApiController
+    {
+        [Route("api/AjaxAPI/AjaxMethod")]
+        [HttpPost]
+        public List<Customer> AjaxMethod(CustomerModel customer)
+        {
+            NorthwindEntities entities = new NorthwindEntities();
+            return (from c in entities.Customers.Take(10)
+                    where c.ContactName.StartsWith(customer.Name)
+                    select c).ToList();
+        }
+    }
+}
---
+++ AngularJS_Web_API_MVC/Models/CustomerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularJS_Web_API_MVC.Models
+{
+    public class CustomerModel
+    {
+        ///<summary>
+        /// Gets or sets Name.
+        ///</summary>
+        public string Name { get; set; }
+    }
+}
